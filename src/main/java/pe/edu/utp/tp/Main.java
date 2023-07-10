c476508e20e3128d19724f59578afe81eb9ad01e
WuteLink <71299264+WuteLink@users.noreply.github.com>
2023-07-09 20:52:54 -0500

    Add files via upload

---
+++ src/main/java/pe/edu/utp/tp/Main.java
@@ -0,0 +1,102 @@
+package pe.edu.utp.tp;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        String txtSubMenu;
+        Scanner lector = new Scanner(System.in);
+        OpcionPrincipal opcionPrincipal;
+        OpcionSecundario opcionSecundario;
+        boolean salirMenuPrincipal = false;
+        String opcPrincipal = """
+                --------------------------------------------------------
+                MENU PRINCIPAL
+                --------------------------------------------------------
+                1. Número de eventos sísmicos por año dado un rango de años.
+                2. Número de eventos sísmicos por mes dado un año.
+                3. Número de eventos sísmicos por mes dados un rango de magnitudes y un año
+                4. Número de eventos sísmicos por cada hora dado un año.
+                0. FIN DEL PROGRAMA
+                --------------------------------------------------------
+                Ingrese opción [1 – 4]""";
+        String opcSecundario = """
+                --------------------------------------------------------
+                %s
+                --------------------------------------------------------
+                1. Imprimir por pantalla.
+                2. Exportar a archivo plano.
+                0. Volver al Menú Principal
+                --------------------------------------------------------
+                Ingrese opción [1-2]
+                """;
+
+        System.out.println(opcPrincipal);
+
+        opcionPrincipal = OpcionPrincipal.values()[lector.nextByte()];
+
+        while (salirMenuPrincipal){
+            switch (opcionPrincipal){
+                case FIN_DE_PROGRAMA:
+                    salirMenuPrincipal = true;
+                    break;
+                case RANGO_DE_ANIOS:
+                    txtSubMenu = "MÓDULO 01 – EVENTOS POR RANGO DE AÑOS";
+                    System.out.printf(opcSecundario, txtSubMenu);
+                    opcionSecundario = OpcionSecundario.values()[lector.nextByte()];
+                    switch (opcionSecundario){
+                        case MENU_PRINCIPAL:
+                            salirMenuPrincipal = false;
+                            break;
+                        case IMPRIMIR_PANTALLA:
+                            salirMenuPrincipal = false;
+                            break;
+                        case EXPORTAR_ARCHIVO:
+
+                            break;
+                    }
+                    break;
+                case MES_POR_ANIO:
+                    txtSubMenu = "MÓDULO 02 – EVENTOS POR MES DADO UN AÑO";
+                    System.out.printf(opcSecundario,txtSubMenu);
+                    opcionSecundario = OpcionSecundario.values()[lector.nextByte()];
+                    break;
+                case MES_POR_RANGO_MAGNITUDES_Y_ANIO:
+
+                    txtSubMenu = "MÓDULO 03 – EVENTOS POR MES DADOS UN RANGO DE MAGNITUDES Y UN AÑO";
+                    System.out.printf(opcSecundario,txtSubMenu);
+                    opcionSecundario = OpcionSecundario.values()[lector.nextByte()];
+                    break;
+                case HORA_POR_ANIO:
+
+                    txtSubMenu = "MÓDULO 04 – EVENTOS POR MES DADO UN AÑO";
+                    System.out.printf(opcSecundario,txtSubMenu);
+                    opcionSecundario = OpcionSecundario.values()[lector.nextByte()];
+                    break;
+                default:
+                    System.out.println("Opción ingresada no es válida");
+            }
+        }
+
+/*
+        String linea;
+        try {
+            BufferedReader archivo = new BufferedReader(new FileReader("C:\\Users\\Alvar\\Documents\\Catálogo Sísmico Perú 1960-2021 (DATASET).csv"));
+            linea = archivo.readLine();
+            while (linea != null){
+                System.out.println(linea);
+                linea = archivo.readLine();
+            }
+        }catch (FileNotFoundException e){
+            System.out.println("Archivo no encontrado");
+        }catch (IOException e){
+            System.out.println("No hay lineas para leer");
+        }
+        */
+
+    }
+}
---
+++ src/main/java/pe/edu/utp/tp/OpcionSecundario.java
@@ -0,0 +1,7 @@
+package pe.edu.utp.tp;
+
+public enum OpcionSecundario {
+    MENU_PRINCIPAL,
+    IMPRIMIR_PANTALLA,
+    EXPORTAR_ARCHIVO
+}
